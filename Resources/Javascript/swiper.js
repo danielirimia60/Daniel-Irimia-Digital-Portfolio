cc3b38af85f154ca3ade746352034328ee2cf56a
Daniel Irimia <91692405+danielirimia60@users.noreply.github.com>
2022-04-03 16:29:19 +0100

    Add files via upload

---
+++ Resources/Javascript/swiper.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect: "cube",
+        grabCursor: true,
+        cubeEffect: {
+          shadow: false,
+          slideShadows: false,
+        },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
